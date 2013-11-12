d9a49e43e6c97a033296f9b67d739c491febcbfd
mail@thomaseger.de <mail@thomaseger.de>
2013-11-12 17:47:00 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Media::Application.config.secret_token = 'b35f0363686ffc34804992f724fc1a7acc1a3c9eb0d753cf70fa76d08eec7afa1c0bdc52192f4bb90e7b44e617f7a56169b4f921397d277c1c5b3c6a0f5bd5b4'
