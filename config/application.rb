0d95aa72ca2bfe4e56fb8331c3609abebd91d84a
Thomas Eger <mail@thomaseger.de>
2013-12-16 22:44:22 +0100

    I18n.enforce_available_locales set to true.

--- config/application.rb
+++ config/application.rb
@@ -69,5 +69,7 @@ module Media
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+     I18n.enforce_available_locales = true
   end
 end
