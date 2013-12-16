bcd176d38992e7a03fecbe5b8fbaeea4c3032017
Thomas Eger <mail@thomaseger.de>
2013-12-16 22:41:48 +0100

    Fixed cucumber scenarios for password confirmation message.

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -169,11 +169,11 @@ Then /^I should see a missing password message$/ do
 end
 
 Then /^I should see a missing password confirmation message$/ do
-  page.should have_content "Password doesn't match confirmation"
+  page.should have_content "Password confirmation doesn't match"
 end
 
 Then /^I should see a mismatched password message$/ do
-  page.should have_content "Password doesn't match confirmation"
+  page.should have_content "Password confirmation doesn't match"
 end
 
 Then /^I should see a signed out message$/ do
