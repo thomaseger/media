8c54a57e24b4d908d69c61ab643407f5cdc50c24
Thomas Eger <mail@thomaseger.de>
2013-12-17 16:39:52 +0100

    Removed secret_token.rb from repository.

--- config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Media::Application.config.secret_token = 'b35f0363686ffc34804992f724fc1a7acc1a3c9eb0d753cf70fa76d08eec7afa1c0bdc52192f4bb90e7b44e617f7a56169b4f921397d277c1c5b3c6a0f5bd5b4'
-Media::Application.config.secret_key_base = '601af50745d4203e0f39e2ea39884599acac2a4e138e9d002b20f44fc3b814514658f9dc756ac2dd87925a85f4135bf44c5206cf930a09e7f88a366d2d0de3b4'
